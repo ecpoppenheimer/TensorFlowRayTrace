681e3e71f5dd1c151673bceca45b054ca3f47296
JoshKarpel <josh.karpel@gmail.com>
2019-02-26 12:22:04 -0600

    run black over all the code

--- setup.py
+++ setup.py
@@ -6,17 +6,15 @@ from setuptools import setup
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-    name = 'tfrt',
-    version = "0.1.0",
-    long_description = Path('README.md').read_text(),
-    long_description_content_type = "text/markdown",
-    url = 'https://github.com/ecpoppenheimer/TensorFlowRayTrace',
-    classifiers = [
+    name="tfrt",
+    version="0.1.0",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ecpoppenheimer/TensorFlowRayTrace",
+    classifiers=[
         "Intended Audience :: Science/Research",
         "Operating System :: OS Independent",
     ],
-    packages = [
-        'tfrt',
-    ],
-    install_requires = Path('requirements.txt').read_text().splitlines(),
+    packages=["tfrt"],
+    install_requires=Path("requirements.txt").read_text().splitlines(),
 )
--- tfrt/spectrumRGB.py
+++ tfrt/spectrumRGB.py
@@ -5,65 +5,62 @@ Based on
 """
 
 import numpy as np
-select=np.select
-power=np.power
-transpose=np.transpose
-arange=np.arange
+
+select = np.select
+power = np.power
+transpose = np.transpose
+arange = np.arange
+
 
 def factor(wl):
     return select(
-        [ wl > 700.,
-          wl < 420.,
-          True ],
-        [ .3+.7*(780.-wl)/(780.-700.),
-          .3+.7*(wl-380.)/(420.-380.),
-          1.0 ] )
+        [wl > 700.0, wl < 420.0, True],
+        [
+            0.3 + 0.7 * (780.0 - wl) / (780.0 - 700.0),
+            0.3 + 0.7 * (wl - 380.0) / (420.0 - 380.0),
+            1.0,
+        ],
+    )
+
 
 def raw_r(wl):
     return select(
-        [ wl >= 580.,
-          wl >= 510.,
-          wl >= 440.,
-          wl >= 380.,
-          True ],
-        [ 1.0,
-          (wl-510.)/(580.-510.),
-          0.0,
-          (wl-440.)/(380.-440.),
-          0.0 ] )
+        [wl >= 580.0, wl >= 510.0, wl >= 440.0, wl >= 380.0, True],
+        [1.0, (wl - 510.0) / (580.0 - 510.0), 0.0, (wl - 440.0) / (380.0 - 440.0), 0.0],
+    )
+
 
 def raw_g(wl):
     return select(
-        [ wl >= 645.,
-          wl >= 580.,
-          wl >= 490.,
-          wl >= 440.,
-          True ],
-        [ 0.0,
-          (wl-645.)/(580.-645.),
-          1.0,
-          (wl-440.)/(490.-440.),
-          0.0 ] )
+        [wl >= 645.0, wl >= 580.0, wl >= 490.0, wl >= 440.0, True],
+        [0.0, (wl - 645.0) / (580.0 - 645.0), 1.0, (wl - 440.0) / (490.0 - 440.0), 0.0],
+    )
+
 
 def raw_b(wl):
     return select(
-        [ wl >= 510.,
-          wl >= 490.,
-          wl >= 380.,
-          True ],
-        [ 0.0,
-          (wl-510.)/(490.-510.),
-          1.0,
-          0.0 ] )
+        [wl >= 510.0, wl >= 490.0, wl >= 380.0, True],
+        [0.0, (wl - 510.0) / (490.0 - 510.0), 1.0, 0.0],
+    )
+
 
 gamma = 0.80
-def correct_r(wl):
-    return power(factor(wl)*raw_r(wl),gamma)
-def correct_g(wl):
-    return power(factor(wl)*raw_g(wl),gamma)
-def correct_b(wl):
-    return power(factor(wl)*raw_b(wl),gamma)
 
-ww=arange(380.,781.)
+
+def correct_r(wl):
+    return power(factor(wl) * raw_r(wl), gamma)
+
+
+def correct_g(wl):
+    return power(factor(wl) * raw_g(wl), gamma)
+
+
+def correct_b(wl):
+    return power(factor(wl) * raw_b(wl), gamma)
+
+
+ww = arange(380.0, 781.0)
+
+
 def rgb():
-    return transpose([correct_r(ww),correct_g(ww),correct_b(ww)])
+    return transpose([correct_r(ww), correct_g(ww), correct_b(ww)])
